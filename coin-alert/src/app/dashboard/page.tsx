d93b46226c0718b398c4bda0b76a1fba6c77c5f5
Gkhoury17 <gekh5755@colorado.edu>
2025-02-12 22:51:39 -0700

    Registered service worker on dashboard page.

--- coin-alert/src/app/dashboard/page.tsx
+++ coin-alert/src/app/dashboard/page.tsx
@@ -15,6 +15,22 @@ export default function Dashboard() {
   const {user, loading, userData} = useAuth();
   const [error, setError] = useState("");
 
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('/firebase-messaging-sw.js')
+                .then((registration) => {
+                    console.log('Service Worker registered:', registration);
+                })
+                .catch((error) => {
+                    console.error('Service Worker registration failed:', error);
+                });
+        });
+    } else {
+      console.error("No service worker")
+    }
+}, []);
+
     // 🔹 Function to Validate Solana Address
     const isValidSolanaAddress = (address: string): boolean => {
       try {
