8bddffaf2e72cbcf561b36043d98b012e2cc4853
Gkhoury17 <gekh5755@colorado.edu>
2025-02-12 23:20:17 -0700

    Removed import scripts.

--- coin-alert/public/firebase-messaging-sw.js
+++ coin-alert/public/firebase-messaging-sw.js
@@ -1,6 +1,3 @@
-importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
-
 
 firebase.initializeApp({
   apiKey: "AIzaSyBhXEAWxOXuRgkFAQdkkN7WYI4j7iyZCPE",
