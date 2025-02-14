6396e48718ded0b4a88a5d6be4aee71dd9efa703
Gkhoury17 <gekh5755@colorado.edu>
2025-02-13 22:15:33 -0700

    Updated icons and home page.

--- coin-alert/src/app/page.tsx
+++ coin-alert/src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 import styles from "./page.module.css";
 
@@ -35,60 +34,16 @@ export default function Home() {
       </button>
         <h2>Setting up notifications</h2>
         <p>CoinAlert will alert you on all devices when your coins change drastically in price. Below are checks to ensure your devices are properly set up to receive notifications.</p>
-        <h3>Web/Mac/Desktop</h3>
-        <p>For web browsers, navigate to Settings  Notifications and confirm notifications are enabled. You can enable notifications only for CoinAlert if you so choose.</p>
+        <h3>Web/Mac/Windows</h3>
+        <p>For web browsers, navigate to Settings → Notifications and confirm notifications are enabled. You can enable notifications only for CoinAlert if you so choose.</p>
         <p>Go to your system settings and confirm notifications are enabled for your browser.</p>
-        <p>If on Mac you may be in Focus or some other Do Not Disturb mode, which in this case you will only receive criticial alerts</p>
+        <p>If on Mac you may be in Focus, Do Not Disturb or some other mode that is preventing you to enable notifications.</p>
 
         <h3>Mobile</h3>
-        <p>...</p>
-
-
-
+        <p>On IOS, view this page on Safari and click the share icon. Click "Add to Home Screen". Upon re-opening the app you will be prompted to enable notifications. Select yes. You can customize notifications anytime in your notification settings under the Siren app.</p>
       </main>
       <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        2025 Siren Copywrite
       </footer>
     </div>
   );
